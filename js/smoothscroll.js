c7c278b1e2fc3b891b30446b12b3dc2b3d360d59
Nkomo <dumi_nkomo@yahoo.co.uk>
2015-12-08 23:33:18 +0000

    Nearly there
    Need to :
    - Fix Broken Header
    - Complete Footer
    - Complete responsiveness
    - Include contact me page/share links

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -37,3 +37,47 @@ $("a.navbar-brand[href^='#']").on('click', function(e) {
      });
 
 });
+
+
+
+$("a.back-to-top[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top -50
+     }, 700, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+$("a.portfolio[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top -50
+     }, 700, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+
